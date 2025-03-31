2e9529a4c5564fd3c3ade8a24d2340b5dea9a5d7
kelledge46 <kelledge6@outlook.com>
2025-03-31 15:36:56 -0600

    Update streamlit_app.py
    deleted the "helpful links" I had still had in there

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col 
 
-helpful_links = [
-    "https://docs.streamlit.io",
-    "https://docs.snowflake.com/en/developer-guide/streamlit/about-streamlit",
-    "https://github.com/Snowflake-Labs/snowflake-demo-streamlit",
-    "https://docs.snowflake.com/en/release-notes/streamlit-in-snowflake"
-]
 
 # Write directly to the app
 st.title("My Parents New Healthy Diner")
