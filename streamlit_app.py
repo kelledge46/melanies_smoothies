12215cad191d27bd1a72253ad913b88af6c0eb49
kelledge46 <kelledge6@outlook.com>
2025-03-31 15:32:28 -0600

    Update streamlit_app.py
    added json() to last line

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,6 +61,7 @@ if time_to_insert:
     st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
 
+# New section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
